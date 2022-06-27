a3173752eb301b29a37b763fe014850b1aad102a
Travis Hutchison <travis@nullinsight.com>
2022-06-26 21:00:47 -0600

    Added docker files for setup

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('db/', include('shipserv.urls')),
     path('login/', views.login_view, name="login_view"),
     path('logout/', views.logout_view, name="logout_view"),
-
 ]
